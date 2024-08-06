2bb19e82b4592a7248e7b2cd25578a342b601f95
ramosgarciamorrisonbladimir <ramosgarciamorrisonbladimir@gmail.com>
2024-08-05 19:57:14 -0600

    cafcat

---
+++ catcaf/prof.edwin/relog.js
@@ -0,0 +1,4 @@
+const hora = document.getElementById('hora');
+const dia = document.getElementById('dia');
+
+const  meses = ["enero","febrero","marzo","abril","Mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
